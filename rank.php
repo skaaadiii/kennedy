d661dc7cdc8775c9ed29fe86e6a79a1addbfc11b
skaaadiii <61456821+skaaadiii@users.noreply.github.com>
2022-01-23 15:38:28 +0800

    Update rank.php

--- rank.php
+++ rank.php
@@ -8,7 +8,17 @@
   <meta charset="utf-8" />
   <meta name="viewport" content="user-scalable=no, width=device-width, initial-scale=1.0" />
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-</head>
+  <script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?c54299e13013c97226cc406b3cd581ea";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
+
+  </head>
 
 <body>
   <?php
